2017bd150ff06b0921347e6017fc334d72222ddd
Dmitriy Berezin <d.berezin@tencoins.org>
2023-06-27 14:04:48 +0300

    wip

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -32,7 +32,7 @@ contract Lock is OwnableUpgradeable, UUPSUpgradeable {
     }
 
     function test2() public pure returns (uint256) {
-        return 2;
+        return 4;
     }
 
     function hello() public pure returns (string memory) {
